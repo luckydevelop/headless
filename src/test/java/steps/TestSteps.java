2eacfbb2b288256da70ebd6260a633f63b40bc11
Oleg <luckydev3000@gmail.com>
2017-09-18 20:05:37 -0500

    dfghdfgh

--- src/test/java/steps/TestSteps.java
+++ src/test/java/steps/TestSteps.java
@@ -7,9 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.firefox.GeckoDriverService;
-import org.openqa.selenium.remote.DesiredCapabilities;
+
 
 import java.io.File;
 
