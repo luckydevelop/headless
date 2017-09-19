4b313a792e8bd73a5a1557a9075388c4f235063a
Oleg <luckydev3000@gmail.com>
2017-09-18 22:39:02 -0500

    fgdfhhfg

--- src/test/java/steps/TestSteps.java
+++ src/test/java/steps/TestSteps.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -16,14 +17,19 @@ public class TestSteps {
     @Given("^Test$")
     public void test() throws Throwable {
 
-        System.setProperty("webdriver.gecko.driver", "src/test/resources/drivers/geckodriver.exe");
+       //System.setProperty("webdriver.gecko.driver", "C:\\Users\\luckyPC\\Desktop\\Temp\\ert\\src\\test\\resources\\drivers\\geckodriver.exe");
        // File pathToBinary = new File("D:\\Programs\\Firefox47\\firefox.exe");
        // FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
        // FirefoxProfile firefoxProfile = new FirefoxProfile();
        // FirefoxDriver driver = new FirefoxDriver(ffBinary,firefoxProfile);
 
+        DesiredCapabilities capabilities=DesiredCapabilities.firefox();
+        capabilities.setCapability("marionette", true);
+        WebDriver driver = new FirefoxDriver(capabilities);
 
-       WebDriver driver = new FirefoxDriver();
+      //  WebDriver driver = new FirefoxDriver(capa);
+
+       //WebDriver driver = new FirefoxDriver();
         driver.get("http://www.toolsqa.com");
         Assert.assertEquals(driver.getTitle(),"QA Automation Tools Tutorial");
         driver.quit();
