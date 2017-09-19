f9a4ea00926ac97f057a51dddcfc623c6d17bf72
Oleg <luckydev3000@gmail.com>
2017-09-18 20:51:11 -0500

    ghjfghj

--- src/test/java/steps/TestSteps.java
+++ src/test/java/steps/TestSteps.java
@@ -16,10 +16,13 @@ public class TestSteps {
     @Given("^Test$")
     public void test() throws Throwable {
 
-        System.setProperty("webdriver.gecko.driver", "C:\\Users\\luckyPC\\Desktop\\Temp\\ert\\src\\test\\resources\\drivers\\geckodriver.exe");
-        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-        capabilities.setCapability("marionette", true);
-        WebDriver driver = new FirefoxDriver(capabilities);
+        File pathToBinary = new File("D:\\Programs\\Firefox47\\firefox.exe");
+        FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
+        FirefoxProfile firefoxProfile = new FirefoxProfile();
+        FirefoxDriver driver = new FirefoxDriver(ffBinary,firefoxProfile);
+
+      //  System.setProperty("webdriver.firefox.driver", "D:\\Programs\\Firefox47\\firefox.exe");
+      //  WebDriver driver = new FirefoxDriver();
         driver.get("http://www.toolsqa.com");
         Assert.assertEquals(driver.getTitle(),"QA Automation Tools Tutorial");
         driver.quit();
