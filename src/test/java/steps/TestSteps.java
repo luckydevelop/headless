5752d65f311a3a0527202422a6e1bbe7aacaf9d9
Oleg <luckydev3000@gmail.com>
2017-09-18 20:07:25 -0500

    sdsdgdfgd

--- src/test/java/steps/TestSteps.java
+++ src/test/java/steps/TestSteps.java
@@ -1,11 +1,7 @@
 package steps;
 
 import cucumber.api.java.en.Given;
-import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 
