587c2fe8a03824280b78db7c14817cbb3f5aebad
Oleg <luckydev3000@gmail.com>
2017-09-18 20:17:05 -0500

    dr

--- src/test/java/steps/TestSteps.java
+++ src/test/java/steps/TestSteps.java
@@ -12,7 +12,7 @@ public class TestSteps {
     @Given("^Test$")
     public void test() throws Throwable {
 
-        String geckoDriverPath = "C:\\Users\\luckyPC\\Desktop\\Temp\\ert\\src\\test\\resources\\drivers\\geckodriver.exe";
+        String geckoDriverPath = "src/test/resources/drivers/geckodriver.exe";
         System.setProperty("webdriver.gecko.driver", geckoDriverPath);
         WebDriver driver = new FirefoxDriver();
         driver.get("http://www.seleniumeasy.com");
