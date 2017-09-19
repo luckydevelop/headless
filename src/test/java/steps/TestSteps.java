9bfa69dbd92d5c4ee0993b3b3cef125a6afb29e4
Oleg <luckydev3000@gmail.com>
2017-09-18 19:59:41 -0500

    12341

--- src/test/java/steps/TestSteps.java
+++ src/test/java/steps/TestSteps.java
@@ -5,27 +5,78 @@ import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.GeckoDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
 
 public class TestSteps {
 
     @Given("^Test$")
     public void test() throws Throwable {
 
-       System.setProperty("webdriver.gecko.driver", "C:\\Users\\luckyPC\\Desktop\\Temp\\ert\\src\\test\\resources\\drivers\\geckodriver.exe");
-       DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-       capabilities.setCapability("marionette", true);
-       WebDriver driver = new FirefoxDriver(capabilities);
-     //  WebDriver driver = new FirefoxDriver(capabilities);
-      // WebDriver driver = new FirefoxDriver();
-       // driver.get("http://www.toolsqa.com");
+        String geckoDriverPath = "C:\\Users\\luckyPC\\Desktop\\Temp\\ert\\src\\test\\resources\\drivers\\geckodriver.exe";
+        System.setProperty("webdriver.gecko.driver", geckoDriverPath);
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.seleniumeasy.com");
+        //String title = driver.getTitle();
+       // System.out.println("Title is " + title );
+      //  driver.quit();
 
-       // WebDriver webDriver = new ChromeDriver();
-        driver.get("https://mvnrepository.com");
-        String title = driver.getTitle();
-        System.out.println(title);
-        Assert.assertEquals("Maven Repository: Search/Browse/Explore",title);
+        //System.setProperty("webdriver.gecko.driver", "D:\\Programs\\Firefox47\\firefox.exe");
+      //  WebDriver driver = new FirefoxDriver();
+
+        //File pathToBinary = new File("D:\\Programs\\Firefox47\\firefox.exe");
+       // FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
+      //  FirefoxProfile firefoxProfile = new FirefoxProfile();
+       // FirefoxDriver driver = new FirefoxDriver(ffBinary,firefoxProfile);
+
+        //WebDriver driver = new FirefoxDriver(firefoxProfile);
+
+//     // System.setProperty("webdriver.gecko.driver", "D:\\Programs\\Firefox47\\firefox.exe");
+//       System.setProperty("webdriver.gecko.driver", "D:\\Programs\\Firefox47\\firefox.exe");
+//       DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+//     capabilities.setCapability("marionette", true);
+//       WebDriver driver = new FirefoxDriver(capabilities);
+//     //   WebDriver driver = new FirefoxDriver();
+//     //  WebDriver driver = new FirefoxDriver(capabilities);
+//      // WebDriver driver = new FirefoxDriver();
+//       // driver.get("http://www.toolsqa.com");
+//
+//       // WebDriver webDriver = new ChromeDriver();
+//
+////        File pathToBinary = new File("C:\\Users\\luckyPC\\Desktop\\Temp\\ert\\src\\test\\resources\\drivers\\geckodriver.exe");
+////       FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
+////        FirefoxProfile firefoxProfile = new FirefoxProfile();
+////        new GeckoDriverService()
+////        WebDriver driver = new FirefoxDriver(ffBinary,firefoxProfile);
+//       // WebDriver driver = new FirefoxDriver();
+//
+//       // File pathToBinary = new File("D:\\Programs\\Firefox47\\firefox.exe");
+//      //  FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
+//       // FirefoxProfile firefoxProfile = new FirefoxProfile();
+//      //  WebDriver driver = new FirefoxDriver();
+//
+//       // WebDriver driver = new GeckoDriverService();
+//
+//       // File pathToBinary = new File("D:\\Programs\\Firefox47\\firefox.exe");
+//       // FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
+//       // FirefoxProfile firefoxProfile = new FirefoxProfile();
+//
+//       // File pathToBinary = new File("D:\\Programs\\Firefox47\\firefox.exe");
+//       // FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
+//       //FirefoxProfile firefoxProfile = new FirefoxProfile();
+//        //WebDriver driver = new FirefoxDriver(ffBinary,firefoxProfile);
+//       // WebDriver driver = new FirefoxDriver();
+
+
+       // driver.get("https://mvnrepository.com");
+       // String title = driver.getTitle();
+       // System.out.println(title);
+      //  Assert.assertEquals("Maven Repository: Search/Browse/Explore",title);
 
 
     }
