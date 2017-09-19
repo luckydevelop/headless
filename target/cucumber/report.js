d9610d393db829ddaf893cb28155574c96920793
Oleg <luckydev3000@gmail.com>
2017-09-19 12:36:18 -0500

    sdsgl;kjl;

---
+++ target/cucumber/report.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("test.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Test",
+  "description": "",
+  "id": "test",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "Test",
+  "description": "",
+  "id": "test;test",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 4,
+  "name": "Test",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "TestSteps.test()"
+});
+formatter.result({
+  "duration": 14471928789,
+  "status": "passed"
+});
+});
