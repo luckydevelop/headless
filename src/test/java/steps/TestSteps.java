38305f5578db443d91a209a4717e83690474ed9e
Oleg <luckydev3000@gmail.com>
2017-09-19 12:45:26 -0500

    sder;

--- src/test/java/steps/TestSteps.java
+++ src/test/java/steps/TestSteps.java
@@ -1,7 +1,8 @@
 package steps;
 
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
-import org.junit.After;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxBinary;
@@ -17,28 +18,18 @@ public class TestSteps {
 
     WebDriver driver;
 
+    @Before
+    public void before(){
+        System.out.println("before HI!!!!!!!!!!!!!!!!!!!!!!!!");
+    }
+
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+
     @Given("^Test$")
-    public void test() throws Throwable {
-
-      // System.setProperty("webdriver.gecko.driver", "C:\\Users\\luckyPC\\Desktop\\Temp");
-       //System.setProperty("webdriver.firefox.driver", "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
-       // File pathToBinary = new File("C:\Users\luckyPC\Desktop\Temp\ert\src\test\resources\drivers\geckodriver.exe");
-       // FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
-       // FirefoxProfile firefoxProfile = new FirefoxProfile();
-       // FirefoxDriver driver = new FirefoxDriver(ffBinary,firefoxProfile);
-
-        //DesiredCapabilities capabilities=DesiredCapabilities.firefox();
-       // capabilities.setCapability("marionette", true);
-        //WebDriver driver = new FirefoxDriver(capabilities);
-
-      //  WebDriver driver = new FirefoxDriver(capa);
-
-        //DesiredCapabilities capabilities=DesiredCapabilities.firefox();
-
-       // capabilities.setCapability("app", "PATH_TO_APP");
-       //WebDriver driver = new FirefoxDriver();
-
-
+    public void test() {
         driver = new FirefoxDriver();
 
         driver.get("http://www.toolsqa.com");
@@ -46,8 +37,13 @@ public class TestSteps {
         System.out.println(driver.getTitle());
         driver.quit();
     }
-    @After
-    public void tearDown(){
-        driver.quit();
+
+    @Given("^Test2$")
+    public void test2()  {
+        driver = new FirefoxDriver();
+        driver.get("http://www.toolsqa.com");
+        Assert.assertEquals(driver.getTitle(),"QA Automation Tools Tutorial2");
     }
+
+
 }
